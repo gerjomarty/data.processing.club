d61160707a178d6b0036c8a979b9730320cfea5d
Gerard Martin <344915+gerjomarty@users.noreply.github.com>
2022-01-06 20:53:30 +0000

    Add some confetti and music for the birthday girl

--- bubb4clock/javascripts/application.js
+++ bubb4clock/javascripts/application.js
@@ -2,6 +2,49 @@ import * as audioModule from "./audio.js"
 import * as settingsModule from "./settings.js"
 import * as clocksModule from "./clocks.js"
 
+import * as confettiModule from "./confetti.js";
+
+function renderConfetti() {
+  const confettiCanvas = document.getElementById("confetti");
+  const confettiSettings = { target: confettiCanvas };
+
+  const c = new confettiModule.ConfettiGenerator(confettiSettings);
+  c.render();
+
+  confettiCanvas.style.display = "block";
+}
+
+function resetConfetti() {
+  const confettiCanvas = document.getElementById("confetti");
+  const confettiSettings = { target: confettiCanvas };
+
+  const c = new confettiModule.ConfettiGenerator(confettiSettings);
+  c.clear();
+
+  confettiCanvas.style.display = "none";
+}
+
+function toggleConfetti() {
+  const confettiCanvas = document.getElementById("confetti");
+
+  if (confettiCanvas.dataset.on == "true") {
+    confettiCanvas.dataset.on = "false";
+    resetConfetti();
+  } else {
+    confettiCanvas.dataset.on = "true";
+    renderConfetti();
+  }
+}
+
+renderConfetti();
+
+const toggleConfettiLink = document.getElementById("toggle-confetti");
+toggleConfettiLink.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  toggleConfetti();
+});
+
 const audioSelect = document.getElementById("audio-select");
 const audioCanvas = document.getElementById("audio");
 
